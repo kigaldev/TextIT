d4eecebc06e126a69bde430d89bb1b3577092f30
kigaldev <kigaldev@gmail.com>
2024-11-13 17:53:05 +0100

    actualizacion codigo
    Inicio con las pruebas y depuraciones de errores

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import TextEditor from './components/TextEditor';
+import Toolbar from './components/Toolbar';
+import FileMenu from './components/FileMenu';
+import StatusBar from './components/StatusBar';
+
+const App = () => {
+  return (
+    <div className="app-container">
+      <header className="app-header">
+        <FileMenu />
+        <Toolbar />
+      </header>
+      <main className="app-main">
+        <TextEditor />
+      </main>
+      <footer className="app-footer">
+        <StatusBar charCount={0} wordCount={0} />
+      </footer>
+    </div>
+  );
+};
+
+export default App;
